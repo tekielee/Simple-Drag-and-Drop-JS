0b4b76253745890152f0ae941ef629e7366367ee
Cuong Le <tekielee@yahoo.com>
2021-03-06 17:43:26 -0800

    Simple Drag and Drop JS

---
+++ app.js
@@ -0,0 +1,31 @@
+const div = document.querySelector('div');
+
+let divX = 50;
+let divY = 50;
+
+div.style.left = divX + 'px';
+div.style.top = divY + 'px';
+
+let dragActive = false;
+let insertDivX;
+let insertDivY;
+
+div.addEventListener('mousedown', (e) => {
+	div.style.backgroundColor = 'steelblue';
+	dragActive = !dragActive;
+	
+	offsetDivX = e.offsetX;
+	offsetDivY = e.offsetY;
+});
+
+document.addEventListener('mouseup', (e) => {
+	if(dragActive) {
+		divX = e.clientX - offsetDivX;
+		divY = e.clientY - offsetDivY;
+	}
+	
+	div.style.left = divX + 'px';
+	div.style.top = divY + 'px';
+	div.style.backgroundColor = 'blue';
+	dragActive = !dragActive;
+});
